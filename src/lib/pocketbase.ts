db99a9b5b8fc2a9e5452e8554be38d37fff5bcb0
Anass ibnouali <anass.ibnou-ali@alithya.com>
2026-03-06 10:28:55 +0000

    fix: Improve docs update

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase'
 
 export const PB_URL = import.meta.env.VITE_PB_URL || (import.meta.env.DEV ? 'http://127.0.0.1:8090' : '')
-
+// Hello
 console.log('PB_URL =>', PB_URL)
 
 const pb = new PocketBase(PB_URL)
